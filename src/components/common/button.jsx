b083ad2b85cac3ee806ca112467e217004ef1339
Ama Nkansah <amankansahstate@gmail.com>
2025-07-20 19:05:19 +0100

    button component fixed

---
+++ src/components/common/button.jsx
@@ -0,0 +1,7 @@
+
+export default function Button () {
+  return (
+<button className="backdrop-blur-md bg-black/50 dark:bg-fuchsia-200 transition-all duration-300 border-zinc-500 px-4 py-2 rounded-full mt-5 -translate-y-85 translate-x-250">
+  <a href="#" className="text-lg font-semibold text-black hover:text-fuchsia-200 dark:hover:text-fuchsia-900 ease-in-out ">View Resume</a>
+</button>
+)};
