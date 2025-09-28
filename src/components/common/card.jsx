b22f640eb62eccbbfb6a5d1cecdca78587f843ff
Ama Nkansah <amankansahstate@gmail.com>
2025-09-28 22:56:20 +0000

    modify files

--- src/components/common/card.jsx
+++
@@ -1,7 +0,0 @@
- 
-export const Card = ({styling,children}) =>{
-return (
-  <div className={` p-6 rounded-2xl backdrop-blur-md border-black/20 bg-black/20 dark:bg-black/50 border dark:border-white/20 shadow-md my-5 ${styling}`}>
-      {children}
-  </div>
-)};
--- src/components/common/menu.jsx
+++
@@ -1,9 +0,0 @@
-import {Menu} from "lucide-react";
-
-export const MenuIcon = ()=> {
-  return(
-   <div>
-    <Menu className="w-6 h-6 text-black dark:text-fuchsia-200 hover:text-fuchsia-600 dark:hover:text-fuchsia-300 mx-88 -translate-y-15 transition md:hidden" />
-   </div>
-  );
-}
